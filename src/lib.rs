b0b3a576c6016f99abcd57da61b6fe7dfdddc7f4
rob-p <rob.patro@gmail.com>
2024-03-28 13:35:03 -0400

    tiny pass success

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use sha2::{Sha512, Digest};
 use base64::{Engine as _, engine::{self, general_purpose}, prelude::BASE64_URL_SAFE};
+use itertools::Itertools;
+use std::iter;
+use serde_json::json;
 
 #[derive(Debug)]
 struct SeqCol {
@@ -28,14 +31,26 @@ impl SeqCol {
     }
 }
 
-pub fn sha512t24u_digest(attr: &[u8], offset: usize) -> Vec<u8> {
-    let mut sd = Sha512::new();
-    sd.update(attr);
-    let digest = sd.finalize();
-    let tdigest_b64us = &BASE64_URL_SAFE.encode(&digest[offset..]);
-    general_purpose::STANDARD.decode(tdigest_b64us).unwrap()
+pub fn sha512t24u_digest(attr: &[u8], offset: usize) -> String {
+    println!("adding {}", std::str::from_utf8(attr).unwrap());
+    let digest = Sha512::digest(attr);
+    println!("digest = {:?}", &digest[..offset]);
+    let tdigest_b64us = BASE64_URL_SAFE.encode(&digest[..offset]);
+    tdigest_b64us
 }
 
+pub fn canonical_rep(inp: &serde_json::Value) -> anyhow::Result<String> {
+    if let Some(om) = inp.as_object() {
+        let mut output_obj = vec![];//serde_json::Object::new();
+        for k in om.keys().sorted() {
+            output_obj.push((k, om[k].clone()))
+        }
+        let output_val: serde_json::Value = output_obj.into_iter().collect();
+        Ok(serde_json::to_string(&output_val)?)
+    } else {
+        Ok(serde_json::to_string(inp)?)
+    }
+}
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -45,6 +60,7 @@ pub fn add(left: usize, right: usize) -> usize {
 mod tests {
     use super::*;
     use noodles_sam;
+    use serde_json::json;
 
     #[test]
     fn from_sam_header_works() {
@@ -63,6 +79,33 @@ mod tests {
         );
 
         println!("s = {s:?}");
+        let lengths = json!({ "lengths" : s.lengths });
+        let names = json!({ "names" : s.names });
+        let seqs = json!({ "sequences" : s.sequences });
+
+        let v = canonical_rep(&lengths["lengths"]).unwrap();
+        println!("{}", v);
+        let h = sha512t24u_digest(&v.as_bytes(), 24);
+        println!("{h}");
+        let v = canonical_rep(&names["names"]).unwrap();
+        let h2 = sha512t24u_digest(&v.as_bytes(), 24);
+        println!("{h2}");
+
+        let v = canonical_rep(&seqs["sequences"]).unwrap();
+        let h3 = sha512t24u_digest(&v.as_bytes(), 24);
+        println!("{h3}");
+
+        let j = json!({
+            "lengths" : h,
+            "names" : h2,
+            "sequences" : h3
+        });
+
+        let v = canonical_rep(&j).unwrap();
+        println!("{v}");
+        let h4 = sha512t24u_digest(&v.as_bytes(), 24);
+        println!("{h4}");
+
     }
 
 
